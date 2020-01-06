94764a79cbe56c04487716bd2da20e62c91fc33d
ChristmasError <Christmas_ZTM@outlook.com>
2020-01-06 10:31:12 +0800

    项目改名MCLog,并封装为.dll

---
+++ src/MCLog/LogBuffer.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <cstdio>
+#include <cstdint>
+#include <ctime>
+
+//日志缓存区节点-循环链表节点
+class LogBuffer
+{
+public:
+    enum BufferStatus
+    {
+        FREE,
+        FULL
+    };
+
+    LogBuffer(uint32_t len) :
+        mStatus(FREE),
+        mPrev(NULL),
+        mNext(NULL),
+        _mTotalLen(len),
+        _mUsedLen(0)
+    {
+        _mTotalLen = len;
+        mCurLogName = new char[MAX_PATH];
+        memset(mCurLogName, 0, MAX_PATH);
+        _mCacheData = new char[len];
+        memset(_mCacheData, 0, len);
+        if (!_mCacheData)
+        {
+            fprintf(stderr, "Error : no space to allocate _mCacheData\n");
+            exit(1);
+        }
+    }
+
+    void Clear()
+    {
+        memset(_mCacheData, 0, _mTotalLen);
+        memset(mCurLogName, 0, MAX_PATH);
+        _mUsedLen = 0;
+        mStatus = FREE;
+    }
+
+    uint32_t AvailableLen() const
+    {
+        return this->_mTotalLen - this->_mUsedLen;
+    }
+
+    bool Empty() const
+    {
+        return _mUsedLen == 0 && mCurLogName[0] == '\0';
+    }
+
+    void AppendLog(const char* log_line, uint32_t len)
+    {
+        if (AvailableLen() < len)
+            return;
+        memcpy(_mCacheData + _mUsedLen, log_line, len);
+        _mUsedLen += len;
+    }
+
+    void WriteFile(FILE* fp)
+    {
+        uint32_t writeLen = fwrite(_mCacheData, 1, _mUsedLen, fp);
+        if (writeLen != _mUsedLen) //写入长度与缓存区文本长度不一致则判断写入文本失败
+        {
+            fprintf(stderr, "Error : write log to disk error,writeLen: %u\n", writeLen);
+        }
+    }
+
+public:
+    BufferStatus    mStatus;   //缓存区状态
+    LogBuffer* mPrev;   //缓存区块前一个缓存区指针
+    LogBuffer* mNext;   //缓存区块下一个缓存区指针
+    char* mCurLogName;    //缓存区块对应日志文件名称
+
+private:
+    uint32_t _mTotalLen;  //单区块空间大小
+    uint32_t _mUsedLen;   //区块已用空间
+    char* _mCacheData; //区块数据 
+
+};
---
+++ src/MCLog/mclog_global.h
@@ -0,0 +1,29 @@
+﻿#ifndef MCLOG_H
+#define MCLOG_H
+
+// 下列 ifdef 块是创建使从 DLL 导出更简单的
+// 宏的标准方法。此 DLL 中的所有文件都是用命令行上定义的 MCLOG_EXPORTS
+// 符号编译的。在使用此 DLL 的
+// 任何项目上不应定义此符号。这样，源文件中包含此文件的任何其他项目都会将
+// MCLOG_API 函数视为是从 DLL 导入的，而此 DLL 则将用此宏定义的
+// 符号视为是被导出的。
+#ifdef MCLOG_EXPORTS
+#define MCLOG_API __declspec(dllexport)
+#else
+#define MCLOG_API __declspec(dllimport)
+#endif
+
+#include <io.h>     //access函数的头文件
+#include <stdarg.h>
+#include <direct.h>
+#include <assert.h>
+#include <windows.h>
+#include <thread>
+#include <tchar.h>
+#ifdef  _DEBUG
+	#include <iostream>
+#endif //  DEBUG
+
+#include "LogBuffer.h"
+
+#endif // MCLOG_H
